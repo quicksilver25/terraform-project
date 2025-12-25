024bcd18f2ab7de074cf0392af3fa0875df9c817
quicksilver25 <smita.dudhane56@gmail.com>
2025-12-25 12:42:29 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
     backend "s3"  {
          bucket          = "cloud-terraform-state-2"
           key            = "terraform.tfstate"
-          region         = "us-west-1"
+          region         = "us-west-2"
           encrypt        = true
           dynamodb_table = "cloudcontainer-tf-table"
    }
