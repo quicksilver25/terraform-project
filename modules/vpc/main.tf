2feb5bd6daec58431d0faf8c34b807d8c029e499
quicksilver25 <smita.dudhane56@gmail.com>
2025-12-25 15:27:19 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -8,22 +8,35 @@ resource "aws_vpc" "main" {
   }
 }
 
+# ------------------------
+# Internet Gateway
+# ------------------------
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
 }
 
+# ------------------------
+# Public Subnets
+# ------------------------
 resource "aws_subnet" "public_subnet" {
   count                   = length(var.public_subnets_cidr)
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnets_cidr[count.index]
-  map_public_ip_on_launch = true
   availability_zone       = element(var.availability_zones, count.index)
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "public-subnet-${count.index}"
   }
 }
 
+# ------------------------
+# Public Route Table
+# ------------------------
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.main.id
 
@@ -43,6 +56,9 @@ resource "aws_route_table_association" "public_subnet_association" {
   route_table_id = aws_route_table.public_route_table.id
 }
 
+# ------------------------
+# Private Subnets
+# ------------------------
 resource "aws_subnet" "private_subnet" {
   count             = length(var.private_subnets_cidr)
   vpc_id            = aws_vpc.main.id
@@ -54,25 +70,37 @@ resource "aws_subnet" "private_subnet" {
   }
 }
 
+# ------------------------
+# NAT Gateway (Single)
+# ------------------------
 resource "aws_eip" "nat" {
-  count  = length(var.public_subnets_cidr)
   domain = "vpc"
+
+  tags = {
+    Name = "nat-eip"
+  }
 }
 
 resource "aws_nat_gateway" "nat_gw" {
-  count         = length(var.public_subnets_cidr)
-  subnet_id     = aws_subnet.public_subnet[count.index].id
-  allocation_id = aws_eip.nat[count.index].id
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_subnet[0].id
 
   depends_on = [aws_internet_gateway.gw]
+
+  tags = {
+    Name = "nat-gateway"
+  }
 }
 
+# ------------------------
+# Private Route Table
+# ------------------------
 resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.main.id
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gw[0].id
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
   }
 
   tags = {
@@ -85,4 +113,3 @@ resource "aws_route_table_association" "private_subnet_association" {
   subnet_id      = aws_subnet.private_subnet[count.index].id
   route_table_id = aws_route_table.private_route_table.id
 }
-
